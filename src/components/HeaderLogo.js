1633d716de9bac04feee51221d0947d6b8ef5604
fabjanvucina <fabjan.vucina@fer.hr>
2021-02-09 17:35:24 +0100

    deleted project content

--- src/components/HeaderLogo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import logo from "../img/crobasket_logo.jpg";
-import "../styles/components/HeaderLogo.css";
-
-const HeaderLogo = () => {
-  return <img src={logo} className="logo" alt="logo" id="logo" />;
-};
-
-export default HeaderLogo;
--- src/contexts/HometownContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const HometownContext = createContext("");
-
-export default HometownContext;
